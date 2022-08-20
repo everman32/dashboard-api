97e9c52ce72f5f9015b20335f48f2cc272dc206d
Victor <vitek.borodyn@gmail.com>
2022-08-20 18:43:38 +0300

    Add an abstraction from a logger implementation

--- src/logger/logger-interface.ts
+++ src/logger/logger-interface.ts
@@ -1,7 +1,5 @@
-import { Logger } from "tslog";
-
 export interface ILogger {
-  logger: Logger;
+  logger: unknown;
   log: (...args: unknown[]) => void;
   error: (...args: unknown[]) => void;
   warn: (...args: unknown[]) => void;
