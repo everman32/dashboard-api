b9d0ecb0a8e45f7a74af1e1c9bafcaff8cc1ebd8
Victor <vitek.borodyn@gmail.com>
2022-08-21 23:26:49 +0300

    Add the ability to handle middleware

--- src/common/base-controller.ts
+++ src/common/base-controller.ts
@@ -27,8 +27,10 @@ export abstract class BaseController {
   bindRoutes(routes: IRoute[]): void {
     routes.forEach((route) => {
       this.logger.log(`[${route.method}] ${route.path}`);
+      const middleware = route.middlewares?.map((m) => m.execute.bind(m));
       const handler = route.func.bind(this);
-      this.router[route.method](route.path, handler);
+      const pipeline = middleware ? [...middleware, handler] : handler;
+      this.router[route.method](route.path, pipeline);
     });
   }
 }
