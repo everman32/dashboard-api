5816f20971ac3b3b964848eff6ea4087d47001df
Victor <vitek.borodyn@gmail.com>
2022-08-22 01:23:58 +0300

    Add transmission of encryption key length via .env

--- src/users/user-service.ts
+++ src/users/user-service.ts
@@ -1,4 +1,6 @@
-import { injectable } from "inversify";
+import { inject, injectable } from "inversify";
+import { IConfigService } from "../config/config-service-interface";
+import { INJECT_TYPES } from "../types.js";
 import { UserLoginDto } from "./dto/user-login-dto";
 import { UserRegisterDto } from "./dto/user-register-dto";
 import { User } from "./user-entity.js";
@@ -6,13 +8,18 @@ import { IUserService } from "./user-service-interface";
 
 @injectable()
 export class UserService implements IUserService {
+  constructor(
+    @inject(INJECT_TYPES.IConfigService) private configService: IConfigService,
+  ) {}
+
   async createUser({
     email,
     name,
     password,
   }: UserRegisterDto): Promise<User | null> {
     const newUser = new User(email, name);
-    await newUser.setPassword(password);
+    const salt = this.configService.get("SALT");
+    await newUser.setPassword(password, Number(salt));
     return null;
   }
 
