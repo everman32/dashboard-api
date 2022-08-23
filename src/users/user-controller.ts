530e987f0d186b5dd49b0baa5ce8b83d90ac5057
Victor <vitek.borodyn@gmail.com>
2022-08-23 03:30:35 +0300

    Change login method according to validation

--- src/users/user-controller.ts
+++ src/users/user-controller.ts
@@ -35,13 +35,16 @@ export class UserController extends BaseController implements IUserController {
     ]);
   }
 
-  login(
+  async login(
     req: Request<{}, {}, UserLoginDto>,
     res: Response,
     next: NextFunction,
-  ): void {
-    this.loggerService.log("login");
-    this.ok(res, "login");
+  ): Promise<void> {
+    const result = await this.userService.validateUser(req.body);
+    if (!result) {
+      return next(new HTTPError(401, "Authorization error"));
+    }
+    this.ok(res, {});
   }
 
   async register(
