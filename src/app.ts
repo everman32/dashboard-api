fe742b6befae51396400c01305cc4a9306394aad
Victor <vitek.borodyn@gmail.com>
2022-08-31 02:32:54 +0300

    Add callbacks to app init and close methods

--- src/app.ts
+++ src/app.ts
@@ -55,11 +55,18 @@ export class App {
     this.useExceptionFilters();
 
     await this.databaseService.connect();
-    this.server = this.app.listen(this.port);
-
-    this.logger.log(`Server is running at port ${this.port}`);
+    this.server = this.app.listen(this.port, () => {
+      this.logger.log(`Server is running at port ${this.port}`);
+    });
   }
 
   close(): void {
+    this.server.close((err) => {
+      if (err) {
+        this.logger.log(`Server closing is failed. ${err.message}`);
+      } else {
+        this.logger.log(`Server is closed`);
+      }
+    });
   }
 }
