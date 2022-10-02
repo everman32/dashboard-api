89ac3fc70fed1d3eac49036467bad01783eb9894
Victor <vitek.borodyn@gmail.com>
2022-10-02 22:12:26 +0300

    Simplify authorization middleware

--- src/common/middleware/auth-middleware.ts
+++ src/common/middleware/auth-middleware.ts
@@ -1,27 +1,22 @@
 import { Request, Response, NextFunction } from "express";
-import { IMiddleware } from "./middleware-interface";
 import { verify } from "jsonwebtoken";
 import { IHttp } from "../http-interface";
 
 export class AuthMiddleware implements IHttp {
-export class AuthMiddleware implements IMiddleware {
   private secret: string;
 
   constructor(secret: string) {
     this.secret = secret;
   }
 
-  execute(req: Request, _res: Response, next: NextFunction): void {
-    if (req.headers.authorization) {
-      verify(
-        req.headers.authorization.split(" ")[1],
-        this.secret,
-        (_err, payload) => {
-          if (payload && typeof payload !== "string") {
-            req.user = payload.email;
-          }
-        },
-      );
+  execute(req: Request, res: Response, next: NextFunction): void {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (token) {
+      verify(token, this.secret, (_err, payload) => {
+        if (payload && typeof payload !== "string") {
+          req.user = payload.email;
+        }
+      });
     }
     next();
   }
